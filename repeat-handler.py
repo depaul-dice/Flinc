d7eb55bca53db4f9c08dffc1944107974dc31163
Raza Ahmad <sra0nasir@gmail.com>
2024-03-21 18:11:02 -0500

    Updated log file and debug statements

--- repeat-handler.py
+++ repeat-handler.py
@@ -6,22 +6,25 @@ import sys
 import subprocess
 
 def sigIntHandler(*_):
+    print("Inside sigIntHandler")
     parent = psutil.Process(os.getpid())
-    print(parent.exe())
+    print("parent:", parent.exe())
     for child in parent.children(recursive=True):
-        print(child.exe())
+        print("child:", child.exe())
         child.send_signal(signal.SIGKILL)
 
 def sigTermHandler(*_):
+    print("Inside sigTermHandler")
     parent = psutil.Process(os.getpid())
-    print(parent.exe())
+    print("parent:", parent.exe())
     for child in parent.children(recursive=True):
-        print(child.exe())
+        print("child:", child.exe())
         child.send_signal(signal.SIGKILL)
 
 
-sys.stdout = open('test.log', 'a')
-sys.stderr = open('test.log', 'a')
+print("Repeat Kernel")
+sys.stdout = open('flinc.log', 'a')
+sys.stderr = open('flinc.log', 'a')
 signal.signal(signal.SIGINT, sigIntHandler)
 signal.signal(signal.SIGTERM, sigTermHandler)
 subprocess.run(sys.argv[1:])
