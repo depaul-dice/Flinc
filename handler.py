8b021dc1482d81e39b2683645c9367fc5bfaf1b8
Raza Ahmad <sra0nasir@gmail.com>
2024-03-21 18:10:28 -0500

    Sciunit process tree created separately
    The Sciunit process tree is now not created as a child process of the signal handler. Rather, it is created as its own process group by using start_new_session=True which calles setsid internally. This allows the Sciunit process tree to continue wrapping its auditing even when the Jupyter server sends a kill signal to the signal handler.

--- handler.py
+++ handler.py
@@ -5,33 +5,34 @@ import psutil
 import sys
 import subprocess
 
+
 def sigIntHandler(*_):
     print("Inside sigIntHandler")
-    parent = psutil.Process(os.getpid())
+    parent = psutil.Process(sciunit_pid)
+    print("parent:", parent.exe())
     for child1 in parent.children(recursive=False):
         print("child1:", child1.exe())
         for child2 in child1.children(recursive=False):
             print("child2:", child2.exe())
-            for child3 in child2.children(recursive=False):
-                print("child3:", child3.exe())
-                if "bin/python" in child3.exe():
-                    child3.send_signal(signal.SIGINT)
+            if "bin/python" in child2.exe():
+                child2.send_signal(signal.SIGTERM)
 
 def sigTermHandler(*_):
     print("Inside sigTermHandler")    
-    parent = psutil.Process(os.getpid())
+    parent = psutil.Process(sciunit_pid)
+    print("parent:", parent.exe())
     for child1 in parent.children(recursive=False):
         print("child1:", child1.exe())
         for child2 in child1.children(recursive=False):
             print("child2:", child2.exe())
-            for child3 in child2.children(recursive=False):
-                print("child3:", child3.exe())
-                if "bin/python" in child3.exe():
-                    child3.send_signal(signal.SIGTERM)
+            if "bin/python" in child2.exe():
+                child2.send_signal(signal.SIGTERM)
 
 
 sys.stdout = open('flinc.log', 'a')
 sys.stderr = open('flinc.log', 'a')
 signal.signal(signal.SIGINT, sigIntHandler)
 signal.signal(signal.SIGTERM, sigTermHandler)
-subprocess.run(sys.argv[1:])
+p = subprocess.Popen(sys.argv[1:], stdout=sys.stdout, stderr=sys.stderr, start_new_session=True)
+sciunit_pid = p.pid
+p.wait()
