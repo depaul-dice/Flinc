a48bc7bb32aefa333b6f2959238e2a8bee0a7810
talha129 <31848225+talha129@users.noreply.github.com>
2024-08-01 21:41:27 +0500

    use last execution in repeat kernel #6

--- repeat-handler.py
+++ repeat-handler.py
@@ -4,6 +4,7 @@ import os
 import psutil
 import sys
 import subprocess
+from sciunit2.records import ExecutionManager
 
 def sigIntHandler(*_):
     print("Inside sigIntHandler")
@@ -21,10 +22,15 @@ def sigTermHandler(*_):
         print("child:", child.exe())
         child.send_signal(signal.SIGKILL)
 
+def get_last_execution():
+    em = ExecutionManager('/root/sciunit/audit-kernel')
+    return em.get_last_id()
 
 print("Repeat Kernel")
 sys.stdout = open('flinc.log', 'a')
 sys.stderr = open('flinc.log', 'a')
 signal.signal(signal.SIGINT, sigIntHandler)
 signal.signal(signal.SIGTERM, sigTermHandler)
+
+sys.argv[5] = 'e' + str(get_last_execution())
 subprocess.run(sys.argv[1:])
