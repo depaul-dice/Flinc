4d663b91ab07d096ebaa2d5b4af8be063f005818
talha129 <31848225+talha129@users.noreply.github.com>
2024-08-05 11:00:19 +0500

    revert to previous commit

--- repeat-handler.py
+++ repeat-handler.py
@@ -4,7 +4,6 @@ import os
 import psutil
 import sys
 import subprocess
-from sciunit2.records import ExecutionManager
 
 def sigIntHandler(*_):
     print("Inside sigIntHandler")
@@ -22,15 +21,9 @@ def sigTermHandler(*_):
         print("child:", child.exe())
         child.send_signal(signal.SIGKILL)
 
-def get_last_execution():
-    em = ExecutionManager(os.path.expanduser('~') + '/sciunit/audit-kernel')
-    return em.get_last_id()
-
 print("Repeat Kernel")
 sys.stdout = open('flinc.log', 'a')
 sys.stderr = open('flinc.log', 'a')
 signal.signal(signal.SIGINT, sigIntHandler)
 signal.signal(signal.SIGTERM, sigTermHandler)
-
-sys.argv[5] = 'e' + str(get_last_execution())
 subprocess.run(sys.argv[1:])
